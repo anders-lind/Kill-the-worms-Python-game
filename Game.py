1fa44587f672446e254439b16c40ade55cda4d64
Anderslind <anders@lind-thomsen.dk>
2018-11-05 08:36:03 +0100

    Basic Physics Engine

--- Game.py
+++ Game.py
@@ -1,46 +1,42 @@
 
 class Game():
     def __init__(self, pygame):
-        #pygame = pygame
-        pygame.init()
-        screen = pygame.display.set_mode((400,300))
+        #pygame.init()
+        #screen = pygame.display.set_mode((1,1))
 
-        self.state = 0
+        #Game status variables
         self.started = False
         self.done = False
+        self.state = 0
 
-        self.movingUp = False
-        self.movingRight = False
-        self.movingDown = False
-        self.movingLeft = False
-
-        self.startKnapHover = False
+        #Keyboard input variables
+        self.w = False
+        self.d = False
+        self.s = False
+        self.a = False
         self.leftClick = False
         self.rightClick = False
         self.middleClick = False
 
-        
-        self.sheetTerrain = pygame.image.load("grass-1-1.png").convert()
-        self.sheetPlayerIdle = pygame.image.load("hr-level1_idle.png").convert()
-        self.sheetPlayerRunning = pygame.image.load("hr-level1_running.png").convert()
-        #Up = (88*0,132*0,88,132) Right = (88*0,132*2,88,132) Down = (88*0,132*4,88,132) Left = (88*0,132*6,88,132)
-        rects = ((88*0,132*0,88,132),(88*0,132*2,88,132),(88*0,132*4,88,132),(88*0,132*6,88,132))
+        #Knapper
+        self.startKnapHover = False
 
-        self.sprites = []
-        for r in rects:
-            image = pygame.Surface(pygame.Rect(r).size).convert()
-            image.blit(self.sheetPlayerRunning, (0, 0), pygame.Rect(r))
-            self.sprites.append(image)
+        #Player variables
+        self.lastMovement = None
+        self.pX = 0
+        self.pY = 0
+        self.playerPos = [self.pX,self.pY]
+        self.pVX = 0
+        self.pVY = 0
+        self.playerVel = [self.pVX,self.pVY]
 
-
-        self.test = False
-        self.test2 = False
-        self.testTal = 0
+        #Sprites
+        self.playerSprites = []
 
 
     def tick(self,pygame,events,pressed):
         self.input(pygame,events,pressed)
-
+        self.physics()
 
 
     def input(self,pygame,events,pressed):
@@ -54,47 +50,102 @@ class Game():
                 print("key pressed")
                 print(event.key)
                 if event.key == 97:
-                    self.movingLeft = True
+                    self.a = True
                 elif event.key == 100:
-                    self.movingRight = True
+                    self.d = True
                 elif event.key == 119:
-                    self.movingUp = True
+                    self.w = True
                 elif event.key == 115:
-                    self.movingDown = True
+                    self.s = True
 
             if event.type == pygame.KEYUP:
                 print("key lifted")
                 print(event.key)
                 if event.key == 97:
-                    self.movingLeft = False
+                    self.a = False
                 elif event.key == 100:
-                    self.movingRight = False
+                    self.d = False
                 elif event.key == 119:
-                    self.movingUp = False
+                    self.w = False
                 elif event.key == 115:
-                    self.movingDown = False
+                    self.s = False
 
 
         if self.state == 0:
+            #Startknap
             if (100 < mouseX < 100+200 and 75 < mouseY < 75+50):
                 self.startKnapHover = True
-                self.testTal += 10
-                print("startknap hover")
+                if leftClick == 1:
+                    self.state = 1
             else:
                 self.startKnapHover = False
 
-            if self.startKnapHover and self.testTal > 500:
-                self.state = 1
-                print("starter spillet")
-        
-
         if self.state == 1:
             pass
-            #print("State 1")
 
-    def lavKnapper(self):
+
+    def physics(self):
+        if self.state == 1:
+
+            # x-movement
+            if self.d:
+                self.pVX = 10
+                self.lastMovement = "RIGHT"
+            elif self.a:
+                self.pVX = -10
+                self.lastMovement = "LEFT"
+            else:
+                self.pVX *= 0.5
+
+            # y-movement
+            if self.w:
+                self.pVY = -10
+                self.lastMovement = "UP"
+            elif self.s:
+                self.pVY = 10
+                self.lastMovement = "DOWN"
+            else:
+                self.pVY *= 0.5
+
+            if self.d and self.w:
+                self.lastMovement = "UP_RIGHT"
+            if self.a and self.w:
+                self.lastMovement = "UP_LEFT"
+            if self.d and self.s:
+                self.lastMovement = "DOWN_RIGHT"
+            if self.a and self.s:
+                self.lastMovement = "DOWN_LEFT"
+            
+        
+        self.pX += self.pVX
+        self.pY += self.pVY
+
+        
+
+        self.playerPos = [self.pX,self.pY]
+        self.playerVel = [self.pVX,self.pVY]
+        print(self.playerVel)
+        print(self.playerPos)
+
+
+    def sprites(self,pygame):
+        #creates sprite-variables
+        self.sheetTerrain = pygame.image.load("grass-1-1.png").convert()
+        self.sheetPlayerIdle = pygame.image.load("hr-level1_idle.png").convert()
+        self.sheetPlayerRunning = pygame.image.load("hr-level1_running.png").convert()
+
+        #Up = (88*0,132*0,88,132) Up+Right = (88*0,132*1,88,132) Right = (88*0,132*2,88,132) Right+down = (88*0,132*0,88,132) Down = (88*0,132*4,88,132) Left = (88*0,132*6,88,132)
+        playerRects = ((88*0,132*0,88,132),(88*0,132*1,88,132),(88*0,132*2,88,132),(88*0,132*3,88,132),(88*0,132*4,88,132),(88*0,132*5,88,132),(88*0,132*6,88,132),(88*0,132*7,88,132))
+
+        for r in playerRects:
+            image = pygame.Surface(pygame.Rect(r).size).convert()
+            image.blit(self.sheetPlayerRunning, (0, 0), pygame.Rect(r))
+            self.playerSprites.append(image)
+
+    '''
+    def lavKnapper(self,pygame):
         self.knapper = []
-        '''
+        
         WHITE = (255,255,255)
         BLACK = (0,0,0)
         myFont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -103,7 +154,7 @@ class Game():
 
         startKnap = ((200,200,200),(100,75,200,50))
         self.knapper.append(startKnap)
-        '''
+    '''
 
 
 
--- Main.py
+++ Main.py
@@ -2,11 +2,20 @@ import pygame as pygame
 from Game import *
 
 game = Game(pygame)
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() 	
 
 
 def main():
-	screen = pygame.display.set_mode((400,300))
+	if not game.started:
+		#åbner et vindue, Køres kun en gang
+		pygame.init()
+		logo = pygame.image.load("32.png")
+		pygame.display.set_icon(logo)
+		pygame.display.set_caption("Python-Spil")
+		screen = pygame.display.set_mode((800,600))
+		game.started = True
+		game.sprites(pygame)
+		print("game starting")
 
 	while not game.done:
 		events = pygame.event.get()
@@ -42,7 +51,6 @@ def render(screen):
 				textSurface = font1.render('Start', True, (100, 100, 100))
 				screen.blit(textSurface,(100+60,75))
 
-				
 			else:
 				pygame.draw.rect(screen,(180,180,180),(100,75,200,50))
 				textSurface = font1.render('Start', True, (100, 100, 100))
@@ -54,28 +62,49 @@ def render(screen):
 			screen.blit(game.sheetTerrain,(-128*6,128))
 			screen.blit(game.sheetTerrain,(-256*4,256))
 
-			if game.movingUp:
-				screen.blit(game.sprites[0],(100,50))
-			if game.movingRight:
-				screen.blit(game.sprites[1],(100,50))
-			if game.movingDown:
-				screen.blit(game.sprites[2],(100,50))
-			if game.movingLeft:
-				screen.blit(game.sprites[3],(100,50))
+			#diagonal movement
+			if game.w and game.d:
+				screen.blit(game.playerSprites[1],game.playerPos)
+			elif game.s and game.d:
+				screen.blit(game.playerSprites[3],game.playerPos)
+			elif game.s and game.a:
+				screen.blit(game.playerSprites[5],game.playerPos)
+			elif game.w and game.a:
+				screen.blit(game.playerSprites[7],game.playerPos)
+
+			#straightMovemt
+			elif game.w:
+				screen.blit(game.playerSprites[0],game.playerPos)
+			elif game.d:
+				screen.blit(game.playerSprites[2],game.playerPos)
+			elif game.s:
+				screen.blit(game.playerSprites[4],game.playerPos)
+			elif game.a:
+				screen.blit(game.playerSprites[6],game.playerPos)
+
+			#Idle
+			elif game.lastMovement == None:
+				screen.blit(game.playerSprites[4],game.playerPos)
+			elif game.lastMovement == "UP":
+				screen.blit(game.playerSprites[0],game.playerPos)
+			elif game.lastMovement == "UP_RIGHT":
+				screen.blit(game.playerSprites[1],game.playerPos)
+			elif game.lastMovement == "UP_LEFT":
+				screen.blit(game.playerSprites[7],game.playerPos)
+			elif game.lastMovement == "DOWN":
+				screen.blit(game.playerSprites[4],game.playerPos)
+			elif game.lastMovement == "DOWN_RIGHT":
+				screen.blit(game.playerSprites[3],game.playerPos)
+			elif game.lastMovement == "DOWN_LEFT":
+				screen.blit(game.playerSprites[5],game.playerPos)
+			elif game.lastMovement == "RIGHT":
+				screen.blit(game.playerSprites[2],game.playerPos)
+			elif game.lastMovement == "LEFT":
+				screen.blit(game.playerSprites[6],game.playerPos)
 			
 		pygame.display.flip()
 
-	else:
-		#åbner et vindue, Køres kun en gang
-		#pygame.init()
-		logo = pygame.image.load("32.png")
-		pygame.display.set_icon(logo)
-		pygame.display.set_caption("minimal program")
 
-		screen = pygame.display.set_mode((400,300))
-
-		game.started = True
-		print("game starting")
 
 
 
